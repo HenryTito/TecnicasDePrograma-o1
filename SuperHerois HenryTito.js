6f3e2d067ab4fc46ffb44ca0bd74befc719d942e
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2025-08-20 21:02:16 -0300

    Algumas refatorações no código
    Correção de alguns nomes e melhora no retorno da função "descricao"

--- SuperHerois HenryTito.js
+++ SuperHerois HenryTito.js
@@ -1,6 +1,4 @@
 
-
-
 const personagem1 = {
     nome: "Wade Wilson",
     codinome: "DeadPool",
@@ -10,45 +8,33 @@ const personagem1 = {
     força: 75.6,
     resistencia: 80,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
-
-
-
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
 }
-personagem1.descricao();
-
-
-
 
 const personagem2 = {
     nome: "Tony Stark",
     codinome: "Homem de Ferro",
-    armaPrincipal: "Armadura",
+    armaPrincipal: "Armaduras (mark I etc.)",
     armaSecundaria: "Rico, playboy, milionário, gênio etc",
     velocidade: 93,
     força: 71.6,
     resistencia: 82,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
 
 const personagem3 = {
@@ -60,112 +46,94 @@ const personagem3 = {
     força: 82.6,
     resistencia: 65,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
 
-
 const personagem4 = {
-    nome: "Miles morales",
+    nome: "Miles Morales",
     codinome: "Homem Aranha",
     armaPrincipal: "Lançar teias",
-    armaSecundaria: "Super teia",
+    armaSecundaria: "Super força, sentidos aguçados etc",
     velocidade: 98,
     força: 20,
     resistencia: 30,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
 
 const personagem5 = {
-    nome: "Dormamo",
+    nome: "Dormammu",
     codinome: "O barganhador",
     armaPrincipal: "Muito foda no geral",
-    armaSecundaria: "Destruir galáxias",
+    armaSecundaria: "Raios de energia",
     velocidade: 93,
     força: 71.6,
     resistencia: 10000,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
 
-
 const personagem6 = {
-    nome: "One above all",
-    codinome: "One above all",
-    armaPrincipal: "Solar o multiverso marvel",
+    nome: "One Above All",
+    codinome: "One Above All",
+    armaPrincipal: "Solar o multiverso marvel ou criar",
     armaSecundaria: "Não sei",
     velocidade: 50,
     força: 80,
     resistencia: 900,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
-
 
 const personagem7 = {
     nome: "Steve Rogers",
     codinome: "Capitão América",
     armaPrincipal: "Escudo de vibrânio",
-    armaSecundaria: "SuperSoldado experimento x",
+    armaSecundaria: "Soro SuperSoldado (mais força etc)",
     velocidade: 93,
     força: 100,
     resistencia: 100,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
 
 const personagem8 = {
-    nome: "Tchala",
+    nome: "T'Challa",
     codinome: "Pantera Negra",
     armaPrincipal: "Garras",
     armaSecundaria: "SuperForça e velocidade",
@@ -173,18 +141,17 @@ const personagem8 = {
     força: 100,
     resistencia: 82,
     descricao: function(){
-        return "Nome do personagem: " + this.nome + 
-        "Codinome" + this.codinome + 
-        "Arma Principal" + this.armaPrincipal + 
-        "Arma Secundária" + this.armaSecundaria + 
-        "Velocidade" + this.velocidade + 
-        "Força" + this.força + 
-        "Resistencia" + this.resistencia
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome: " + this.codinome + "\n" +
+        "Arma Principal: " + this.armaPrincipal + "\n" +
+        "Arma Secundária: " + this.armaSecundaria + "\n" +
+        "Velocidade: " + this.velocidade + "\n" +
+        "Força: " + this.força + "\n" +
+        "Resistencia: " + this.resistencia
     }
-
-
-
 }
+
+
 
 
 const listas = [personagem1, personagem2, personagem3, personagem4, personagem5, personagem6, personagem7, personagem8]
@@ -196,7 +163,15 @@ for (let i = 0; i < listas.length; i++) {
     for (let j = 0; j < listas.length; j++) {
         if (i === j) continue;
 
+
+        
+
         const comparado = listas[j];
+
+        
+        console.log(atual.descricao());
+        console.log("vs");
+        console.log(comparado.descricao());
 
         if (atual.força > comparado.força) {
             console.log(`${atual.nome} Da um sacode ${comparado.nome}`);
@@ -220,14 +195,8 @@ for (let i = 0; i < listas.length; i++) {
     }
 }
 
+//Menção honrosa: Capitão São José, o GOAT. Sola todos ai.
 
-
-
-
-/*
-    Deve estar certo
-
-*/
 
 
 
